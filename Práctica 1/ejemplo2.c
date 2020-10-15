5984250e24a729a9f68292259a228d4b251091b1
danist7 <daniel.santo-tomas@estudiante.uam.es>
2020-10-15 15:39:08 +0200

    ej2 copiao

--- Práctica 1/ejemplo2.c
+++ Práctica 1/ejemplo2.c
@@ -1,39 +1,28 @@
-#include &lt;stdio.h&gt;
-#include &quot;generacion.h&quot;
-
-
-int main (int argc, char** argv){
+#include <stdio.h>
+#include "generacion.h"
+int main (int argc, char** argv)
+{
   FILE * salida;
   int cuantos_no = 0;
-
-  if (argc != 2) {
-    fprintf (stdout, &quot;ERROR POCOS ARGUMENTOS\n&quot;);
-    return -1;
-  }
-
-  salida = fopen(argv[1],&quot;w&quot;);
+  if (argc != 2) {fprintf (stdout, "ERROR POCOS ARGUMENTOS\n"); return -1;}
+  salida = fopen(argv[1],"w");
   escribir_subseccion_data(salida);
   escribir_cabecera_bss(salida);
-  declarar_variable(salida, &quot;b1&quot;, BOOLEANO, 1);
+  declarar_variable(salida, "b1", BOOLEANO, 1);
   escribir_segmento_codigo(salida);
   escribir_inicio_main(salida);
-
   /* scanf b1; */
-  leer(salida,&quot;b1&quot;,BOOLEANO);
-
+  leer(salida,"b1",BOOLEANO);
   /* printf !b1; */
-  escribir_operando(salida,&quot;b1&quot;,1);
+  escribir_operando(salida,"b1",1);
   no(salida,1,cuantos_no++);
   escribir(salida,0,BOOLEANO);
-
   /* printf !!b1; */
-  escribir_operando(salida,&quot;b1&quot;,1);
+  escribir_operando(salida,"b1",1);
   no(salida,1,cuantos_no++);
   no(salida,0,cuantos_no++);
-
   escribir(salida,0,BOOLEANO);
   escribir_fin(salida);
-  
   fclose(salida);
   return 0;
 }
